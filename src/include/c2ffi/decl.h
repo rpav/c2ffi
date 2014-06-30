803fa0273c34badea841439f4e25d7a1e73b70bd
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-07-01 00:13:07 +0300

    Add support for function storage class attribute.

--- src/include/c2ffi/decl.h
+++ src/include/c2ffi/decl.h
@@ -115,11 +115,15 @@ namespace c2ffi {
     class FunctionDecl : public Decl, public FieldsMixin {
         Type *_return;
         bool _is_variadic;
+        bool _is_inline;
+        clang::StorageClass _storage_class;
         bool _is_objc_method;
         bool _is_class_method;
     public:
-        FunctionDecl(std::string name, Type *type, bool is_variadic)
-            : Decl(name), _return(type), _is_variadic(is_variadic),
+        FunctionDecl(std::string name, Type *type, bool is_variadic, bool is_inline,
+                     clang::StorageClass storage_class)
+            : Decl(name), _return(type), _is_variadic(is_variadic), _is_inline(is_inline),
+              _storage_class(storage_class),
               _is_class_method(false), _is_objc_method(false) { }
 
         virtual void write(OutputDriver &od) const { od.write((const FunctionDecl&)*this); }
@@ -127,6 +131,8 @@ namespace c2ffi {
         virtual const Type& return_type() const { return *_return; }
 
         bool is_variadic() const { return _is_variadic; }
+        bool is_inline() const { return _is_inline; }
+        clang::StorageClass storage_class() const { return _storage_class; }
         bool is_objc_method() const { return _is_objc_method; }
         void set_is_objc_method(bool val) {
             _is_objc_method = val;

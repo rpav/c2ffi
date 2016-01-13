1c7dcd8a69e9b23f6cf8f14ee7ea9dc6d5cc5c9c
Ryan Pavlik <rpavlik@gmail.com>
2016-01-13 15:44:45 -0600

    Ignore IndirectFieldDecls

--- src/AST.cpp
+++ src/AST.cpp
@@ -104,6 +104,7 @@ void C2FFIASTConsumer::HandleDecl(clang::Decl *d, const clang::NamedDecl *ns) {
 
     /* C/C++ */
     else if_cast(x, clang::FieldDecl, d);
+    else if_cast(x, clang::IndirectFieldDecl, d);
     else if_cast(x, clang::CXXMethodDecl, d);
     else if_cast(x, clang::FunctionTemplateDecl, d);
     else if_cast(x, clang::FunctionDecl, d) PROC;

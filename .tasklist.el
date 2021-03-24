289163f0cc4aad57bd7a3b0afaa71dda7028bd9e
Ryan Pavlik <rpavlik@gmail.com>
2021-03-24 09:15:10 -0500

    update for my build stuff

--- .tasklist.el
+++ .tasklist.el
@@ -2,20 +2,20 @@
   (build
    (:name "Build %p")
    (:window "Build/Run %p")
-   (:command "grunt" "cmake_build:c2ffi"))
+   (:command "grunt" "cmake_build --current-only"))
   (run
    (:name "Run %p")
    (:window "Build/Run %p")
-   (:command "grunt" "cmake_build_run:c2ffi"))
+   (:command "grunt" "cmake_build_run --target=c2ffi"))
   (clean
    (:name "Clean %p")
    (:window "Build/Run %p")
-   (:command "grunt" "cmake_build:clean"))
+   (:command "grunt" "cmake_clean --current-only"))
   (cmake
    (:name "Configure CMake")
    (:window "Build/Run %p")
-   (:command "grunt" "cmake_config"))
+   (:command "grunt" "cmake_config --current-only"))
   (cmake-clear-cache
    (:name "Clear Cache and Configure CMake")
    (:window "Build/Run %p")
-   (:command "grunt" "cmake_config --clear-cache"))))
+   (:command "grunt" "cmake_config --clear-cache --current-only"))))

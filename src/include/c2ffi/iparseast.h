94f622784a4aefa23c901c0897cc7abe5d18fff6
Eric Eaton <zaimzet@gmail.com>
2021-03-31 19:51:16 -0700

    Change preamble of iparseast.* files

--- src/include/c2ffi/iparseast.h
+++ src/include/c2ffi/iparseast.h
@@ -1,14 +1,23 @@
-//===--- ParseAST.h - Define the ParseAST method ----------------*- C++ -*-===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-//
-//  This file defines the clang::ParseAST method.
-//
-//===----------------------------------------------------------------------===//
+/*  -*- c++ -*-
+
+    This file is part of c2ffi.
+
+    c2ffi is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    c2ffi is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with c2ffi.  If not, see <http://www.gnu.org/licenses/>.
+
+    This code mostly originates from clang, with some small modifications.
+    https://clang.llvm.org/
+*/
 
 #ifndef C2FFI_PARSE_PARSEAST_H
 #define C2FFI_PARSE_PARSEAST_H
